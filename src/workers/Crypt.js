0861b81a8b846f1cb0e9a2d09940d455df5b70e6
Haris Rahman <iharisrahman@yahoo.com>
2021-03-30 04:59:30 +0530

    encrypt logic done, gh-pages added

--- src/workers/Crypt.js
+++ src/workers/Crypt.js
@@ -1,17 +1,30 @@
+function random(min, max)
+{
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function Crypt(text, encrypt)
 {
-	console.log(text, encrypt);
+	if (text.length < 1 || (text.length < 2 && encrypt === false)) return "";
 
-	// const result = "sx";
-	// text.split('').forEach(letter =>
-	// {
-	// 	letter.charCodeAt(0);
-	// 	String.fromCharCode(65);
-	// });
+	const key = encrypt ? random(33, 123) : text.charCodeAt(0);
 
+	let result = text.split('').map((letter, index) =>
+	{
+		if (index == 0 && !encrypt) return "";
 
-	// return result;
-	return "blah";
+		const letter_code = letter.charCodeAt(0);
+
+		const encrypted_code = encrypt ? (letter_code + key) : (letter_code - key);
+		const encrypted_letter = String.fromCharCode(encrypted_code)
+
+		return ((index == 0 && encrypt) ? String.fromCharCode(key) : "") + encrypted_letter;
+	}).join('');
+
+	console.log(result);
+
+	// return text;
+	return result;
 }
 
 export default Crypt;
